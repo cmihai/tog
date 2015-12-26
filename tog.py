798638e98670c6275cdf3b32a372b3082b660d89
Mihai <cmihai@noreply.users.github.com>
2015-12-26 16:29:27 +0100

    Replace tabs with spaces

--- tog.py
+++ tog.py
@@ -6,15 +6,15 @@ from datetime import datetime
 @click.command()
 @click.option('--us', is_flag=True, help='Show time with microsecond precision.')
 def cli(us):
-	for line in sys.stdin:
-		if us:
-			format = '%Y-%m-%d %H:%M:%S.%f'
-		else:
-			format = '%Y-%m-%d %H:%M:%S'
-		timestr = datetime.now().strftime(format)
+    for line in sys.stdin:
+        if us:
+            format = '%Y-%m-%d %H:%M:%S.%f'
+        else:
+            format = '%Y-%m-%d %H:%M:%S'
+        timestr = datetime.now().strftime(format)
 
-		print('[{}] {}'.format(timestr, line.rstrip()))
+        print('[{}] {}'.format(timestr, line.rstrip()))
 
 
 if __name__ == '__main__':
-	cli()
+    cli()
